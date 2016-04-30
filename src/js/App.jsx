da5f8bf166b175a1708949073bff5d0e46d5148b
Kevin Qi <iqnivek@gmail.com>
2016-04-30 18:37:28 -0400

    Add navbar

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -28,43 +28,49 @@ const Project = ({ name, image, url, links, children }) => {
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h1>Kevin Qi</h1>
+      <div>
+        <nav className="navbar navbar-light bg-faded m-b-2">
+          <div className="container">
+            <a className="navbar-brand" href="/">Kevin Qi</a>
+          </div>
+        </nav>
 
-        <h2>Projects</h2>
-        <section>
-          <Project
-            name="react-calendar-heatmap"
-            image="assets/react_calendar_heatmap.png"
-            links={[
-              { text: 'Github repo', url: 'https://github.com/patientslikeme/react-calendar-heatmap', icon: 'fa-github' },
-              { text: 'Demo page', url: 'http://patientslikeme.github.io/react-calendar-heatmap/' },
-            ]}
-          >
-            An open source calendar heatmap component inspired by github's contribution graph, available on NPM. Built with React and SVG.
-          </Project>
+        <div className="container">
+          <h2>Projects</h2>
+          <section>
+            <Project
+              name="react-calendar-heatmap"
+              image="assets/react_calendar_heatmap.png"
+              links={[
+                { text: 'Github repo', url: 'https://github.com/patientslikeme/react-calendar-heatmap', icon: 'fa-github' },
+                { text: 'Demo page', url: 'http://patientslikeme.github.io/react-calendar-heatmap/' },
+              ]}
+            >
+              An open source calendar heatmap component inspired by github's contribution graph, available on NPM. Built with React and SVG.
+            </Project>
 
-          <Project
-            name="EventMapper"
-            image="assets/eventmapper.png"
-            links={[
-              { text: 'View site', url: 'http://eventmapper.net' }
-            ]}
-          >
-            A site that puts concerts on a map, ranked by popularity. Built with Rails and the Songkick API.
-          </Project>
+            <Project
+              name="EventMapper"
+              image="assets/eventmapper.png"
+              links={[
+                { text: 'View site', url: 'http://eventmapper.net' }
+              ]}
+            >
+              A site that puts concerts on a map, ranked by popularity. Built with Rails and the Songkick API.
+            </Project>
 
-          <Project
-            name="Save Tabbed Images"
-            image="assets/save_tabbed_images.png"
-            links={[
-              { text: 'Github repo', url: 'https://github.com/iqnivek/save_tabbed_images', icon: 'fa-github' },
-              { text: 'Download in chrome web store', url: 'https://chrome.google.com/webstore/detail/save-tabbed-images/hhcoikfhkkadkgklepjkfgafmjoggefh', icon: 'fa-chrome' },
-            ]}
-          >
-            Open source chrome extension for downloading images in tabs super quick. Built with React and Webpack.
-          </Project>
-        </section>
+            <Project
+              name="Save Tabbed Images"
+              image="assets/save_tabbed_images.png"
+              links={[
+                { text: 'Github repo', url: 'https://github.com/iqnivek/save_tabbed_images', icon: 'fa-github' },
+                { text: 'Download in chrome web store', url: 'https://chrome.google.com/webstore/detail/save-tabbed-images/hhcoikfhkkadkgklepjkfgafmjoggefh', icon: 'fa-chrome' },
+              ]}
+            >
+              Open source chrome extension for downloading images in tabs super quick. Built with React and Webpack.
+            </Project>
+          </section>
+        </div>
       </div>
     );
   }
