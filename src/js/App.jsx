977183a204cd3c0fbbbf527b5091ed63cfa15ce0
Kevin Qi <iqnivek@gmail.com>
2016-04-30 17:47:52 -0400

    add content

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -3,9 +3,11 @@ import '../styles/components/app';
 import React from 'react';
 
 const Project = ({ name, image, url, children }) => (
-  <div className="row">
+  <div className="row m-b-1">
     <div className="col-sm-6">
-      <img className="img-fluid" src={image} />
+      <div className="text-xs-center">
+        <img className="img-fluid img-rounded" src={image} />
+      </div>
     </div>
     <div className="col-sm-6">
       <h2>{name}</h2>
@@ -20,12 +22,22 @@ class App extends React.Component {
       <div className="container">
         <h1>Kevin Qi</h1>
 
-        <Project
-          name="EventMapper"
-          image="assets/eventmapper.png"
-        >
-          Browsing concert listings can be a mess, so I built a site that maps out concerts ranked by popularity. When you're in a city with a lot going on, it's a lot easier to find the right show.
-        </Project>
+        <h2>Projects</h2>
+        <section>
+          <Project
+            name="EventMapper"
+            image="assets/eventmapper.png"
+          >
+            Browsing concert listings can be a mess, so I built a site that maps out concerts ranked by popularity. Built on Rails and uses the Songkick API.
+          </Project>
+
+          <Project
+            name="Save Tabbed Images"
+            image="assets/save_tabbed_images.png"
+          >
+            Open source chrome extension for downloading images in tabs super quick. Built with react and webpack.
+          </Project>
+        </section>
       </div>
     );
   }
