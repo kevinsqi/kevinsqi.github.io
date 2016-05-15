d1509f0f67949c7f35408dfa88abc7c115878ca7
Kevin Qi <iqnivek@gmail.com>
2016-05-14 23:42:55 -0400

    Tweak header link styles

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -32,10 +32,10 @@ class App extends React.Component {
         <nav className="navbar navbar-dark m-b-2">
           <div className="container">
             <a className="navbar-brand stylized" href="/">Kevin Qi</a>
-            <div className="nav navbar-nav pull-sm-right">
+            <div className="nav navbar-nav pull-sm-right stylized">
               <a className="nav-item nav-link" href="http://iqnivek.github.io/blog/">Blog</a>
               <a className="nav-item nav-link" href="https://github.com/iqnivek">Github</a>
-              <a className="nav-item nav-link" href="assets/kevin_qi_resume.pdf">Resume <i className="fa fa-file-pdf-o"></i></a>
+              <a className="nav-item nav-link" href="assets/kevin_qi_resume.pdf">Resume</a>
             </div>
           </div>
         </nav>
