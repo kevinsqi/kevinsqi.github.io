ffb30c1dafbe8eba8371b76292446e10b04c59cb
Kevin Qi <iqnivek@gmail.com>
2016-05-22 19:29:53 -0400

    Remove ?_k= param from hashHistory

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,17 @@ import './styles/base';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import About from './js/About';
 import App from './js/App';
 
+// remove ?_k= param from hashHistory
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={appHistory}>
     <Route path="/" component={App} />
     <Route path="/about" component={About} />
   </Router>
