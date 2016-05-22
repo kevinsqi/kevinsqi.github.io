87e2c88ca43660f98d691c49b728bde9fa6c4719
Kevin Qi <iqnivek@gmail.com>
2016-05-22 00:46:14 -0400

    Swap out raleway with open sans, fix nav link vertical alignment

--- src/js/Nav.jsx
+++ src/js/Nav.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router';
 const Nav = () => (
   <nav className="navbar navbar-dark m-b-2">
     <div className="container">
-      <Link to="/" className="navbar-brand stylized">Kevin Qi</Link>
+      <Link to="/" className="navbar-brand">Kevin Qi</Link>
 
-      <div className="nav navbar-nav pull-sm-right stylized">
+      <div className="nav navbar-nav pull-sm-right">
         <Link to="/about" className="nav-item nav-link">About</Link>
         <a className="nav-item nav-link" href="http://iqnivek.github.io/blog/">Blog</a>
         <a className="nav-item nav-link" href="assets/kevin_qi_resume.pdf">Resume</a>
