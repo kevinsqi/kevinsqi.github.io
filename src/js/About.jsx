a24c5ada197364c684326aa605f7b94313a26aad
Kevin Qi <iqnivek@gmail.com>
2016-05-22 00:24:27 -0400

    Add gravatar

--- src/js/About.jsx
+++ src/js/About.jsx
@@ -4,6 +4,12 @@ import Page from './Page';
 const About = () => (
   <Page>
     <h1>About</h1>
+
+    <img
+      className="img-circle"
+      src="http://www.gravatar.com/avatar/8192416018e38bce99ad92b85f918ef1?s=200"
+      alt="Kevin Qi"
+    />
   </Page>
 );
 
