389cdee09fd7470b4927c2e12293f3421628d079
Kevin Qi <iqnivek@gmail.com>
2018-07-22 14:22:59 -0700

    WIP porting styles/nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Kevin Qi',
   },
   plugins: ['gatsby-plugin-react-helmet'],
-}
+};
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const Nav = () => (
+  <nav className="navbar navbar-dark mb-2">
+    <div className="container">
+      <Link to="/" className="navbar-brand">
+        Kevin Qi
+      </Link>
+
+      <div className="nav navbar-nav float-xs-right">
+        <Link to="/about" className="nav-item nav-link" activeClassName="active">
+          About
+        </Link>
+        <Link to="/resume" className="nav-item nav-link" activeClassName="active">
+          Resume
+        </Link>
+        <a className="nav-item nav-link" href="http://iqnivek.github.io/blog/">
+          Blog
+        </a>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Nav;
