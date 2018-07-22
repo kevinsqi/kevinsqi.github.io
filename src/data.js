f5ac25d346729349f03378b02634e440449620a4
Kevin Qi <iqnivek@gmail.com>
2018-07-22 14:49:25 -0700

    port resume

---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const URLS = {
+  EVENTMAPPER_SITE: 'http://eventmapper.net',
+  PLM_IOS_APP: 'https://itunes.apple.com/us/app/connect-patientslikeme-control/id955272281',
+  REACT_CALENDAR_HEATMAP_GITHUB: 'https://github.com/patientslikeme/react-calendar-heatmap',
+  SAVE_TABBED_IMAGES_GITHUB: 'https://github.com/iqnivek/save_tabbed_images',
+};
+
+export const PROJECTS = {
+  EVENTMAPPER:
+    'Maps out popular concerts in your city. Personal project built with Rails and the Songkick API.',
+  REACT_CALENDAR_HEATMAP:
+    "An open source calendar heatmap component inspired by github's contribution graph, available on NPM. Built with React and SVG.",
+  SAVE_TABBED_IMAGES:
+    'An open source chrome extension for downloading images in tabs super quick, with 6,000+ weekly users. Personal project built with React.',
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Nav from '../components/Nav';
-
-const URLS = {
-  EVENTMAPPER_SITE: 'http://eventmapper.net',
-  PLM_IOS_APP: 'https://itunes.apple.com/us/app/connect-patientslikeme-control/id955272281',
-  REACT_CALENDAR_HEATMAP_GITHUB: 'https://github.com/patientslikeme/react-calendar-heatmap',
-  SAVE_TABBED_IMAGES_GITHUB: 'https://github.com/iqnivek/save_tabbed_images',
-};
-
-const PROJECTS = {
-  EVENTMAPPER:
-    'Maps out popular concerts in your city. Personal project built with Rails and the Songkick API.',
-  REACT_CALENDAR_HEATMAP:
-    "An open source calendar heatmap component inspired by github's contribution graph, available on NPM. Built with React and SVG.",
-  SAVE_TABBED_IMAGES:
-    'An open source chrome extension for downloading images in tabs super quick, with 6,000+ weekly users. Personal project built with React.',
-};
+import { PROJECTS, URLS } from '../data';
 
 const Project = ({ name, image, url, links, children }) => {
   const primaryUrl = links[0].url;
