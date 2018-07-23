7b47d39119234b06d3baa84d4c1a43c8d907720a
Kevin Qi <iqnivek@gmail.com>
2018-07-22 20:23:44 -0700

    add GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Kevin Qi',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-44564619-1',
+        head: false,
+      },
+    },
+  ],
 };
